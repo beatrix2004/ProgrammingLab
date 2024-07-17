c8ec91e522bf4272e4b45ae8f208725bac07dc39
beatrix2004 <150357792+beatrix2004@users.noreply.github.com>
2024-07-17 17:51:34 +0200

    Add files via upload

---
+++ num.py
@@ -0,0 +1,10 @@
+my_list=[1,2,3,4]
+my_list.reverse()
+print(my_list)
+
+
+#except Exception as e = eccetto ci sia un errore errore-->allora fai questo
+#e=istanza di value error
+#le eccezioni sono gerarchiche (prima exception, poi le altre)
+
+#
